314cba0f56d51f73c2203524e7108dd41ef419d4
eadpearce <e.pearce@tutanota.com>
2018-02-07 12:50:05 +0000

    Added semicolons to appease linter

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 
-var path = require('path')
-var express = require('express')
-var nunjucks = require('nunjucks')
-var routes = require('govuk-elements/app/routes.js')
-var app = express()
-var bodyParser = require('body-parser')
-var config = require('govuk-elements/app/config.js')
-var port = (process.env.PORT || 3000)
-var IS_HEROKU = process.env.hasOwnProperty('IS_HEROKU')
+var path = require('path');
+var express = require('express');
+var nunjucks = require('nunjucks');
+var routes = require('./routes.js');
+var app = express();
+var bodyParser = require('body-parser');
+var config = require('govuk-elements/app/config.js');
+var port = (process.env.PORT || 3000);
+var IS_HEROKU = process.env.hasOwnProperty('IS_HEROKU');
 
-module.exports = app
+module.exports = app;
 
 // Application settings
-app.set('view engine', 'html')
+app.set('view engine', 'html');
 
 // Set the location of the views and govuk_template layout file
 var appViews = [
   path.join(__dirname, '/node_modules/govuk-elements/app/views'),
-  path.join(__dirname, '/node_modules/govuk_template_jinja/views/layouts')
-]
+  path.join(__dirname, '/demo/views')
+];
 
 // Tell nunjucks we are using express to serve the templates within
 // the views defined in appViews
@@ -27,39 +27,39 @@ nunjucks.configure(appViews, {
   autoescape: true,
   watch: true,
   noCache: true
-})
+});
 
 // Middleware to serve static assets
-app.use('/public', express.static(path.join(__dirname, '/export_elements/static/export_elements')))
-app.use('/public', express.static(path.join(__dirname, '/node_modules/govuk_template_jinja/assets')))
-app.use('/public', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit')))
+app.use('/public', express.static(path.join(__dirname, '/export_elements/static/export_elements')));
+app.use('/public', express.static(path.join(__dirname, '/node_modules/govuk_template_jinja/assets')));
+app.use('/public', express.static(path.join(__dirname, '/node_modules/govuk_frontend_toolkit')));
 
 // Support for parsing data in POSTs
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 
 // send assetPath to all views
 app.use(function (req, res, next) {
-  res.locals.asset_path = '/public/'
-  next()
-})
+  res.locals.asset_path = '/public/';
+  next();
+});
 
 // Add variables that are available in all views
 app.use(function (req, res, next) {
-  res.locals.cookieText = config.cookieText
-  next()
-})
+  res.locals.cookieText = config.cookieText;
+  next();
+});
 
 // routes (found in routes.js)
 
-routes.bind(app, '/public/')
+routes.bind(app, '/public/');
 
 // start the app
 
 app.listen(port, function () {
   if (!IS_HEROKU) {
-    console.log('Listening on port ' + port + '   url: http://localhost:' + port)
+    console.log('Listening on port ' + port + '   url: http://localhost:' + port);
   }
-})
+});
